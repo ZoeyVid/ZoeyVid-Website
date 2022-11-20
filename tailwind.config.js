22e8d6b6caf7a28b9c2c602dd0ce0defceff3212
Zoey <zoey@z0ey.de>
2022-11-20 21:19:23 +0100

    Init
    Signed-off-by: Zoey <zoey@z0ey.de>

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  darkMode: 'true',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
