164d68386ea159e512408a0dcaf269c5172ea6c0
Zoey <zoey@z0ey.de>
2022-11-28 20:15:54 +0100

    Update tailwind.config.js
    Signed-off-by: Zoey <zoey@z0ey.de>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   ],
   darkMode: 'true',
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
