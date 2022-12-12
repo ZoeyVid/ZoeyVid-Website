7e270076b9f07f3e26502516bbd9edf833d19a6d
David <david@davidcraft.de>
2022-12-12 15:45:41 +0000

    Neuer Test
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/githubTest.js
+++ assets/js/githubTest.js
@@ -1,9 +1,9 @@
-function test() {
-    fetch("https://api.github.com/users/zoeyvid")
-      .then((response) => {
-        return response.json()
-      })
-      .then((json) => {
-        console.log(json.public_repos)
-      })
-  }
+function countAllRepos() {
+  fetch("https://zoeyvid.de/assets/repos/index.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      console.log(Object.keys(json.shareInfo[i]).length)
+    })
+}
