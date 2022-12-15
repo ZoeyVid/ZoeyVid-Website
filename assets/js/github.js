e5a0f0a0bc92743307489785cbd6570ff33edd78
David <contact@davidcraft.de>
2022-12-15 15:40:21 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

---
+++ assets/js/github.js
@@ -0,0 +1,21 @@
+function countAllRepos() {
+  fetch("https://zoeyvid.de/assets/repos/index.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      console.log(Object.keys(json).length)
+    })
+}
+
+function getAllRepos() {
+  fetch("https://zoeyvid.de/assets/repos/index.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      json.forEach(element => {
+        console.log(element)
+      });
+    })
+}
--- assets/js/githubTest.js
+++
@@ -1,9 +0,0 @@
-function countAllRepos() {
-  fetch("https://zoeyvid.de/assets/repos/index.json")
-    .then((response) => {
-      return response.json()
-    })
-    .then((json) => {
-      console.log(Object.keys(json).length)
-    })
-}
