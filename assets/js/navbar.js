38d0dfa347fba434322fad1c5ff82307e9b5fec2
David <contact@davidcraft.de>
2022-12-22 15:57:19 +0000

    Debug
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,7 @@
 function navbarLoad() {
 document.getElementById("navbar").classList.add("pb-4");
 document.getElementsByTagName('noscript')[0].remove();
+console.log(document.location.pathname)
 if(document.location.pathname == "/"){
   document.getElementById("navbar").classList.remove("pb-4");
   document.getElementById("navbarStartseite").classList.add("bg-gray-900");
