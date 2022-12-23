519ec54eb34889a77974e0e9f757f21d3d2d75b8
David <contact@davidcraft.de>
2022-12-23 15:23:22 +0000

    Test with Copilot design
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/github.js
+++ assets/js/github.js
@@ -14,9 +14,9 @@ function getAllRepos() {
       return response.json()
     })
     .then((json) => {
-      json.forEach(element => {
-        console.log(element)
-      });
+      Object.forEach(element => {
+        document.getElementById("projekte").innerHTML += "<a href='https://zoeyvid.de/projekt?projekt=" + element + "'><div class='bg-gray-800 rounded-lg p-4 m-2'><h1 class='text-2xl font-bold'>" + element + "</h1><p class='text-gray-300'>" + json[element].description + "</p></div></a>"
+      })
     })
 }
 
