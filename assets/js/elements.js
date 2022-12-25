f7ee69c6e28398f63c2b985b7ed1868c233c45f4
Zoey <zoey@z0ey.de>
2022-12-25 20:55:12 +0100

    Update elements.js
    Signed-off-by: Zoey <zoey@z0ey.de>

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -26,6 +26,9 @@ function elementLoad() {
     document.getElementById("navbarProjekteMobile").classList.remove("hover:bg-gray-700");
     document.getElementById("navbarProjekteMobile").classList.remove("hover:text-white");
   }
+  if (document.location.pathname.startsWith("/projekte")) {
+    document.getElementById("footer").classList.remove("mt-auto");
+  }
 }
 
 function toggleMobile() {
