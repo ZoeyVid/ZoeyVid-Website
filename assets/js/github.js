4a4f0e2aae65f309cb5d683f8a85764fccdd0669
David <contact@davidcraft.de>
2022-12-30 15:44:28 +0000

    So?
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/github.js
+++ assets/js/github.js
@@ -22,7 +22,7 @@ function getAllRepos() {
       Object.keys(json).forEach(element => {
         console.log(json[element].topics)
         json[element].topics.forEach(topic => {
-          document.getElementById(element + "Topics").innerHTML += "<span class='bg-gray-700 rounded-lg p-1 mr-1'>" + topic + "</span>"
+          document.getElementById(element + "Topics").innerHTML += "<span class='bg-gray-700 rounded-lg p-1 mr-1 mt-2'>" + topic + "</span>"
         })
         if(json[element].fork) {
           document.getElementById(element + "Headline").innerText += " (Fork)"
