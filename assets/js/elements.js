626bf8051892a6cf4bda3e2f36f9835a485a1268
David <contact@davidcraft.de>
2023-01-12 19:34:31 +0100

    Update elements.js
    Signed-off-by: David <contact@davidcraft.de>

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -52,7 +52,7 @@ function changeExternalLinks() {
       link.href = ""
       link.onclick = function () {
         if (confirm("Willst du wirklich zoeyvid.de verlassen, und auf " + String(externLink) + " gehen?") == true) {
-          window.location.href = new URL(externLink)
+          window.location.replace(externLink)
         }
       };
     }
