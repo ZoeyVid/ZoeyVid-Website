daab7e808e5be5e2516dc83515f7f95c1909e550
David <contact@davidcraft.de>
2023-01-15 15:34:01 +0000

    AH
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -54,7 +54,7 @@ function changeExternalLinks() {
           window.location.href = link.href
         }
       };
-      ink.removeAttribute("href");
+      link.removeAttribute("href");
     }
   });
 }
