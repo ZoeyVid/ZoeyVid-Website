f5b4fb6c4f3635199173ef0398a371173458b014
David <contact@davidcraft.de>
2023-01-19 15:54:59 +0000

    Ah
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -46,8 +46,7 @@ function git() {
 function changeExternalLinks() {
   const links = document.querySelectorAll("a");
   links.forEach(link => {
-    console.log(link.id)
-    if (!link.href.startsWith("https://zoeyvid.de") || link.id == "git") {
+    if (!link.href.startsWith("https://zoeyvid.de") && !link.id == "git") {
       link.onclick = function () {
          if (confirm("Willst du wirklich zoeyvid.de verlassen, und auf " + link.href + " gehen?") == true) {
           return true;
