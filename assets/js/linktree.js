a4468feef08e74a5d75e14a6cdfc131fa64beda0
David <contact@davidcraft.de>
2023-01-21 15:37:00 +0000

    AHHHHH
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -1,4 +1,5 @@
 function test() {
+    document.getElementById("linktree").classList.add("bg-gradient-to-r");
     document.getElementById("linktree").classList.add("from-cyan-500");
     document.getElementById("linktree").classList.add("to-blue-500");
     document.getElementById("linktree").classList.add("w-screen");
