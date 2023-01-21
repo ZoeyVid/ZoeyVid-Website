550fb185d03faac79113665e28d8cb077321ff81
David <contact@davidcraft.de>
2023-01-21 15:49:56 +0000

    Geht das so?
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -1,7 +1,5 @@
 function test() {
-    document.getElementById("linktree").classList.add("bg-gradient-to-r");
-    document.getElementById("linktree").classList.add("from-cyan-500");
-    document.getElementById("linktree").classList.add("to-blue-500");
-    document.getElementById("linktree").classList.add("w-screen");
-    document.getElementById("linktree").classList.add("h-screen");
+    document.getElementsByTagName('body')[0].classList.add("bg-gradient-to-r");
+    document.getElementsByTagName('body')[0].classList.add("from-cyan-500");
+    document.getElementsByTagName('body')[0].classList.add("to-blue-500");
 }
