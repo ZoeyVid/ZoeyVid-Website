2b3f089c22fbcffecebf94dd280edebfb33df903
David <contact@davidcraft.de>
2023-01-21 15:31:07 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

---
+++ assets/js/linktree.js
@@ -0,0 +1,6 @@
+function test() {
+    document.getElementById("Linktree").classList.add("from-cyan-500");
+    document.getElementById("Linktree").classList.add("to-blue-500");
+    document.getElementById("Linktree").classList.add("w-screen");
+    document.getElementById("Linktree").classList.add("h-screen");
+}
