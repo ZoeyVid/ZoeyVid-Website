5f35a4aec98e3b0287efaa8729e082aab6c037cf
David <contact@davidcraft.de>
2023-01-22 15:29:25 +0000

    So?
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -1,7 +1,7 @@
 function loadView() {
     const urlParams = new URLSearchParams(window.location.search);
     //https://zoeyvid.de/social/view/?person=david
-    if (urlParams.get("person") !== "zoey" || urlParams.get("person") !== "zoeyvid" || urlParams.get("person") !== "david") alert(urlParams.get("person"))
+    if (urlParams.get("person") !== "zoey" && urlParams.get("person") !== "zoeyvid" && urlParams.get("person") !== "david") alert(urlParams.get("person"))
     document.getElementsByTagName('body')[0].classList.add("bg-gradient-to-r");
     document.getElementsByTagName('body')[0].classList.add("from-cyan-500");
     document.getElementsByTagName('body')[0].classList.add("to-blue-500");
