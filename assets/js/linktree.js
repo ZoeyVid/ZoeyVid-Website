17c3b0b5b6e0d5e751a86607d7be527f6e36d8f4
David <contact@davidcraft.de>
2023-01-22 15:24:29 +0000

    Test this
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -1,4 +1,6 @@
-function test() {
+function loadView() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get("person") !== "zoey" || urlParams.get("person") !== "zoeyvid" || urlParams.get("person") !== "david") window.location.href = "https://zoeyvid.de/"
     document.getElementsByTagName('body')[0].classList.add("bg-gradient-to-r");
     document.getElementsByTagName('body')[0].classList.add("from-cyan-500");
     document.getElementsByTagName('body')[0].classList.add("to-blue-500");
