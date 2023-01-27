9e0a4fd10dfae9e201ea5bae4e4a6bda41c049c4
David <contact@davidcraft.de>
2023-01-27 15:56:47 +0000

    Design Change 2
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -12,7 +12,7 @@ function loadView() {
         document.getElementById("name").innerHTML = json.name;
         document.getElementById("bild").src = json.profilbild;
         json.links.forEach(element => {
-          document.getElementById("links").innerHTML += '<a href="' + element.url + '" class="block w-full text-center text-white text-2xl font-bold py-6 px-12 rounded-full hover:shadow-lg">' + element.name + '</a>';
+          document.getElementById("links").innerHTML += '<a href="' + element.url + '" class="text-center text-white border-white text-2xl font-bold py-6 px-12 rounded hover:shadow-lg">' + element.name + '</a>';
         });
     })
 }
