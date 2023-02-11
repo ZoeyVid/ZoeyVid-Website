3459c99a3ad82e8bae8aea3edf5cc3adbc425490
David <contact@davidcraft.de>
2023-02-11 15:39:17 +0000

    AHHHH schon wieder
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -12,7 +12,7 @@ function loadView() {
         document.getElementById("name").innerHTML = json.name;
         document.getElementById("bild").src = json.profilbild;
         json.links.forEach(element => {
-          document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><svg class="pre-2"><use href="' + element.icon + '" style="--color_fill: #fffff;"></use></svg>' + element.name + '</div></a>';
+          document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><svg class="pre-2"><use href="' + element.icon + '.svg" style="--color_fill: #fffff;"></use></svg>' + element.name + '</div></a>';
           console.log(element)
         });
         console.log(json)
