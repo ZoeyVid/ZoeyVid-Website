4ac9517d5f9d41c7fb3725510fcdb4ffd35f8d7a
David <contact@davidcraft.de>
2023-02-13 16:02:04 +0000

    Es wird besser
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -12,7 +12,7 @@ function loadView() {
         document.getElementById("name").innerHTML = json.name;
         document.getElementById("bild").src = json.profilbild;
         json.links.forEach(element => {
-          document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><object data="https://zoeyvid.de/assets/bootstrap-icons/' + element.icon + '.svg" style="--color_fill: #fffff;" type="image/svg+xml"></object>' + element.name + '</div></a>';
+          document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><object class="pre-2" data="https://zoeyvid.de/assets/bootstrap-icons/' + element.icon + '.svg" style="--color_fill: #fffff;" type="image/svg+xml"></object>' + element.name + '</div></a>';
           //https://stackoverflow.com/questions/4476526/do-i-use-img-object-or-embed-for-svg-files
           console.log(element)
         });
