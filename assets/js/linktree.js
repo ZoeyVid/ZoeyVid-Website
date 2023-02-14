a9516d4ecf6e748cc7c59730731d60c7974e1c86
David <contact@davidcraft.de>
2023-02-14 15:35:49 +0000

    Zum Test
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -14,6 +14,7 @@ function loadView() {
         json.links.forEach(element => {
           document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><object class="p-2" data="https://zoeyvid.de/assets/bootstrap-icons/' + element.icon + '.svg" style="--color_fill: #fffff;" type="image/svg+xml"></object>' + element.name + '</div></a>';
           document.querySelector("path").setAttribute("stroke", "white");
+          document.querySelector("path").setAttribute("fill", "white");
           //https://stackoverflow.com/questions/4476526/do-i-use-img-object-or-embed-for-svg-files
           console.log(element)
         });
