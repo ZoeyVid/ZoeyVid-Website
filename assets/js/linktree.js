2f828f95a703198cebcd7426d0713215b2f6769f
David <contact@davidcraft.de>
2023-02-15 15:36:39 +0000

    Unnötiges Zeug weg
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -13,12 +13,7 @@ function loadView() {
         document.getElementById("bild").src = json.profilbild;
         json.links.forEach(element => {
           document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><img class="p-2 invert" src="https://zoeyvid.de/assets/bootstrap-icons/' + element.icon + '.svg"></img>' + element.name + '</div></a>';
-          document.querySelector("path").setAttribute("stroke", "white");
-          document.querySelector("path").setAttribute("fill", "white");
-          //https://stackoverflow.com/questions/4476526/do-i-use-img-object-or-embed-for-svg-files
-          console.log(element)
         });
-        console.log(json)
     })
 }
  
