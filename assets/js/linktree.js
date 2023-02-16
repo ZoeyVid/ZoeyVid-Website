d4ec0adeaeaeae550d535c4c1e2c17a5293c895a
David <contact@davidcraft.de>
2023-02-16 15:55:47 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/linktree.js
+++ assets/js/linktree.js
@@ -9,11 +9,37 @@ function loadView() {
         document.getElementsByTagName('body')[0].classList.add("bg-gradient-to-r");
         document.getElementsByTagName('body')[0].classList.add(json.colorFrom);
         document.getElementsByTagName('body')[0].classList.add(json.colorTo);
-        document.getElementById("name").innerHTML = json.name;
         document.getElementById("bild").src = json.profilbild;
+        document.getElementById("name").innerHTML = json.name;
         json.links.forEach(element => {
           document.getElementById("links").innerHTML += '<a href="' + element.url + '"><div class="text-center text-white text-2xl font-bold flex items-center justify-center p-6 rounded-md hover:bg-gray-700"><img class="p-2 invert" h-4/3 w-auto src="https://zoeyvid.de/assets/bootstrap-icons/' + element.icon + '.svg"></img>' + element.name + '</div></a>';
         });
     })
 }
  
+function insertTeamDetails() {
+    fetch("https://zoeyvid.de/assets/socials/zoey.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+        document.getElementById("bildZoey").src = json.profilbild;
+        document.getElementById("nameZoey").innerHTML = json.name;
+    })
+    fetch("https://zoeyvid.de/assets/socials/zoeyvid.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+        document.getElementById("bildZoeyvid").src = json.profilbild;
+        document.getElementById("nameZoeyvid").innerHTML = json.name;
+    })
+    fetch("https://zoeyvid.de/assets/socials/david.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+        document.getElementById("bildDavid").src = json.profilbild;
+        document.getElementById("nameDavid").innerHTML = json.name;
+    })
+}
