028287ec6ddf88b31039876f2b29e7f758f4e516
DavidCraftDev <david@davidcraft.de>
2026-05-30 17:10:28 +0200

    Add service template to index.html and refactor getAllServices function for improved rendering

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,12 +1,18 @@
 function getAllServices() {
-    fetch("https://zoeyvid.de/assets/services.json")
-      .then((response) => {
-        return response.json()
+  fetch("https://zoeyvid.de/assets/services.json")
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      const template = document.getElementById("servicetemplate");
+      const servicesElement = document.getElementById("Services");
+      Object.keys(json).forEach(element => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector(".service-title").textContent = element;
+        clone.querySelector(".service-description").textContent = json[element].description;
+        clone.querySelector(".service-link").href = json[element].link;
+        servicesElement.appendChild(clone);
       })
-      .then((json) => {
-        Object.keys(json).forEach(element => {
-          document.getElementById("Services").innerHTML += "<a href='" + json[element].link  + "' class='text-white'><div class='bg-gray-800 rounded-lg p-4 m-2'><h1 class='text-2xl font-bold hover:underline'>" + element + "</h1><p class='text-gray-300'>" + json[element].description + "</p></div></a>"
-        })
-        return json
-      })
-  }
+      return json
+    })
+}
