4bdbf5fe204720b4f3880162dfdbd9f9ba0f6f34
Zoey <zoey@z0ey.de>
2026-07-04 19:32:08 +0200

    simpler index.json generation

--- assets/js/github.js
+++ assets/js/github.js
@@ -23,7 +23,7 @@ function getAllRepos() {
         clone.querySelector(".repo-link").href = "https://zoeyvid.de/project?project=" + element
         clone.querySelector(".repo-title").textContent = data.name + (Boolean(data.fork) ? " (Fork)" : "")
         clone.querySelector(".repo-description").textContent = data.description
-        clone.querySelector(".repo-spdx-id").textContent = data.spdx_id
+        clone.querySelector(".repo-spdx-id").textContent = data.license
         //data.topics.forEach(topic => {
         //  const topicClone = topicsTemplate.content.cloneNode(true)
         //  topicClone.querySelector(".topic").textContent = topic
